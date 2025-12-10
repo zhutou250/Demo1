924aae6f428ef05a53ccd66f4f18e9c2f060d287
202306120222L <202306120222L@qq.com>
2025-12-11 01:43:16 +0800

    第二次修改HelloGit.java：修改输出内容

--- HelloGit.java
+++ HelloGit.java
@@ -1,6 +1,7 @@
 public class HelloGit {
 	public static void main(String[]args) {
 		//第一次修改：新增注释
-		System.out.println("This is my First GitProject!");
+		//第二次修改：修改输出内容
+		System.out.println("This is my First GitProject! - v2");
 	}
 }
