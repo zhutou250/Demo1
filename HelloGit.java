5e73a22fb7b3f6a673dc9949671dd32d48ebc65d
202306120222L <202306120222L@qq.com>
2025-12-11 02:01:08 +0800

    Master分支:修改输出内容为Master分支专属

--- HelloGit.java
+++ HelloGit.java
@@ -1,7 +1,6 @@
 public class HelloGit {
 	public static void main(String[]args) {
-		//第一次修改：新增注释
-		//第二次修改：修改输出内容
+		//master分支修改
 		System.out.println("This is my First GitProject! - v2");
 	}
 }
